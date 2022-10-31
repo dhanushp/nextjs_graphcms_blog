3a301454583dbffa4d04e882bdb7725c6e31d6c1
dhanushp <dhanushp.work@gmail.com>
2022-10-31 17:00:18 +0530

    Fix

--- components/index.js
+++ components/index.js
@@ -5,7 +5,7 @@ export { default as Header } from './Header';
 export { default as Layout } from './Layout';
 export { default as Author } from './Author';
 export { default as PostDetail } from './PostDetail';
-export { default as CommentsForm } from './Commentsform';
+export { default as CommentsForm } from './CommentsForm';
 export { default as Comments } from './Comments';
 export { default as Loader } from './Loader';
 export { default as FeaturedPostCard } from './FeaturedPostCard';
